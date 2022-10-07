6f043e07c5be81149a1e0ec93c78767cf122dea9
nicholascannon <nicholascannon1@gmail.com>
2022-10-07 22:23:41 +0800

    Add shared AJV validator

---
+++ services/pkg/shared/validation.ts
@@ -0,0 +1,44 @@
+import Ajv, { ErrorObject, JSONSchemaType, ValidateFunction } from 'ajv';
+import ajvFormats from 'ajv-formats';
+
+export class AjvValidator<T> implements Validator<T> {
+    private static AJV = ajvFormats(new Ajv({ allErrors: true }));
+
+    private isValid: ValidateFunction<T>;
+
+    constructor(schema: JSONSchemaType<T>) {
+        this.isValid = AjvValidator.AJV.compile(schema);
+    }
+
+    /**
+     * Validates the object against the schema, throws a ValidationError
+     * if not valid.
+     */
+    validate(object: unknown): T {
+        if (this.isValid(object)) {
+            return object;
+        }
+
+        const errors = this.isValid.errors;
+        if (errors && errors.length > 0) {
+            throw new ValidationError(this.mapAjvErrors(errors));
+        }
+
+        throw new ValidationError(['Object validation failed with no error messages']);
+    }
+
+    private mapAjvErrors(errors: ErrorObject[]): string[] {
+        return errors.map((error) => error.message || `${error.instancePath} violated ${error.keyword} rule`);
+    }
+}
+
+export interface Validator<T> {
+    validate: (object: unknown) => T;
+}
+
+export class ValidationError extends Error {
+    constructor(public errors: string[]) {
+        super('Object validation failed');
+        this.name = this.constructor.name;
+    }
+}
