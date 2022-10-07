36db21ce637da19ba8a3644e3f6f460aaa6eee27
nicholascannon <nicholascannon1@gmail.com>
2022-10-07 21:04:33 +0800

    Client hello world without build tooling

---
+++ client/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('app') as HTMLElement);
+root.render(
+    <React.StrictMode>
+        <h1>Hello, world</h1>
+    </React.StrictMode>
+);
