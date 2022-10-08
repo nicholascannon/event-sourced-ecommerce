f63dae44027e5fc455c426f1e65c35d67c373b33
nicholascannon <nicholascannon1@gmail.com>
2022-10-08 16:53:33 +0800

    Add logging on pool error

--- services/pkg/postgres/db.ts
+++ services/pkg/postgres/db.ts
@@ -1,5 +1,8 @@
 import pg from 'pg';
+import { logger } from '../shared/logger';
 
 export function createPool(config: pg.PoolConfig): pg.Pool {
-    return new pg.Pool(config);
+    const pool = new pg.Pool(config);
+    pool.on('error', (err) => logger.error('Postgres pool error', err));
+    return pool;
 }
