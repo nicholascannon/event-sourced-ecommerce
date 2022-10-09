9a7a91c961f2cc71872e20381cc1cf84f0f1d813
nicholascannon <nicholascannon1@gmail.com>
2022-10-10 06:35:14 +0800

    Send 400 on validation errors

--- services/pkg/http/middleware/error-handler.ts
+++ services/pkg/http/middleware/error-handler.ts
@@ -1,9 +1,14 @@
 import { ErrorRequestHandler } from 'express';
 import { logger } from '../../shared/logger';
+import { ValidationError } from '../../shared/validation';
 
 export const errorHandler: ErrorRequestHandler = (error, _req, res, next) => {
     if (res.headersSent) {
         return next(error);
+    }
+
+    if (error instanceof ValidationError) {
+        return res.status(400).json({ message: error.errors });
     }
 
     logger.error('Internal server error', error);
