a12c273e82a48a4bdc41d71d17241af4be6aafca
nicholascannon <nicholascannon1@gmail.com>
2022-10-15 13:24:29 +0800

    Update the event store to narrow types of events based on stream type

--- services/pkg/event-store/generic-event-store.ts
+++ services/pkg/event-store/generic-event-store.ts
@@ -1,6 +1,10 @@
 import { BaseEvent } from './events';
 
 export interface EventStore<Event extends BaseEvent> {
-    loadStream: (id: string, streamType: Event['streamType']) => Promise<Event[]>;
+    /**
+     * Loads a stream of events and narrows the type down to events only with
+     * that stream type.
+     */
+    loadStream: <E extends Event>(id: string, streamType: E['streamType']) => Promise<E[]>;
     save: (event: Event) => Promise<void>;
 }
