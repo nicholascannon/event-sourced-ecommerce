9d6fb32d5ce6ce9de01f2cc0fafc537ec77794ea
nicholascannon <nicholascannon1@gmail.com>
2022-11-05 11:07:52 +0800

    Add reader and writer to checkout pm

--- services/checkout-pm/config.ts
+++ services/checkout-pm/config.ts
@@ -5,7 +5,19 @@ export const CONFIG = (() => {
     const validator = new AjvValidator<Config>({
         type: 'object',
         properties: {
-            database: {
+            reader: {
+                type: 'object',
+                properties: {
+                    host: { type: 'string' },
+                    user: { type: 'string' },
+                    password: { type: 'string' },
+                    port: { type: 'integer', minimum: 1010, maximum: 65535 },
+                    database: { type: 'string' },
+                },
+                required: ['host', 'user', 'password', 'port', 'database'],
+                additionalProperties: false,
+            },
+            writer: {
                 type: 'object',
                 properties: {
                     host: { type: 'string' },
@@ -18,18 +30,25 @@ export const CONFIG = (() => {
                 additionalProperties: false,
             },
         },
-        required: ['database'],
+        required: ['reader', 'writer'],
         additionalProperties: false,
     });
 
     try {
         return validator.validate({
-            database: {
-                host: process.env.DB_HOST,
-                user: process.env.DB_USER,
-                password: process.env.DB_PASSWORD,
-                port: Number(process.env.DB_PORT),
-                database: process.env.DB_DATABASE,
+            reader: {
+                host: process.env.READER_DB_HOST,
+                user: process.env.READER_DB_USER,
+                password: process.env.READER_DB_PASSWORD,
+                port: Number(process.env.READER_DB_PORT),
+                database: process.env.READER_DB_DATABASE,
+            },
+            writer: {
+                host: process.env.WRITER_DB_HOST,
+                user: process.env.WRITER_DB_USER,
+                password: process.env.WRITER_DB_PASSWORD,
+                port: Number(process.env.WRITER_DB_PORT),
+                database: process.env.WRITER_DB_DATABASE,
             },
         });
     } catch (error) {
@@ -39,11 +58,14 @@ export const CONFIG = (() => {
 })();
 
 export interface Config {
-    database: {
-        host: string;
-        user: string;
-        password: string;
-        port: number;
-        database: string;
-    };
+    reader: DatabaseConfig;
+    writer: DatabaseConfig;
+}
+
+interface DatabaseConfig {
+    host: string;
+    user: string;
+    password: string;
+    port: number;
+    database: string;
 }
