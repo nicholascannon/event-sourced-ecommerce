3b5b9f8992887235473809a7c0af8433cdc94b24
nicholascannon <nicholascannon1@gmail.com>
2023-01-22 11:21:20 +0800

    Client side routing WIP

---
+++ client/src/pages/create-order.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Page } from '../components/page';
+
+export function CreateOrder() {
+    return (
+        <Page title="Create Order">
+            <h1>New order</h1>
+        </Page>
+    );
+}
---
+++ client/src/pages/my-orders.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Page } from '../components/page';
+
+export function MyOrders() {
+    return (
+        <Page title="My Orders">
+            <h1>Your Orders</h1>
+        </Page>
+    );
+}
