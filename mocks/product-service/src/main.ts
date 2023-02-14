8e6d27b5e3a928a26c50d75f4124e4050b1ae56d
nicholascannon <nicholascannon1@gmail.com>
2023-02-14 20:16:16 +0800

    Get all products + product mock CORs

--- mocks/product-service/src/main.ts
+++ mocks/product-service/src/main.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 import { lifecycle } from './lifecycle';
 import validProducts from './valid-products.json';
 
 const app = express();
 app.use(morgan('dev'));
+app.use(
+    cors({
+        origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    })
+);
 
 app.get('/v1/product/:id', (req, res) => {
     const { id } = req.params;
@@ -14,6 +20,10 @@ app.get('/v1/product/:id', (req, res) => {
         return res.sendStatus(404);
     }
     return res.status(200).json({ name: product.name, price: product.price });
+});
+
+app.get('/v1/product', (_req, res) => {
+    return res.json(validProducts);
 });
 
 const server = app.listen(process.env.PORT, () => {
